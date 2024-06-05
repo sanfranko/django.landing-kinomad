c22302f1fead8c12aea144c25508cb43951dfd1f
Alex <31vv31@inbox.ru>
2024-06-06 05:39:28 +0600

    added forms

--- landing/views.py
+++ landing/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.core.mail import send_mail
+from django.conf import settings
 def index(request):
-    return render(request, 'landing/index.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            # Отправка письма (можно настроить по своему усмотрению)
+            send_mail(
+                f'New contact form submission from {name}',
+                message,
+                settings.DEFAULT_FROM_EMAIL,
+                [settings.ADMIN_EMAIL, 'sanfrankooo3@gmail.com'],  # Замените на ваш email
+                fail_silently=False,
+            )
+            return redirect('success')
+    else:
+        form = ContactForm()
+    return render(request, 'landing/index.html', {'form': form})
+
+
+def success(request):
+    return render(request, 'landing/success.html')
--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,11 +117,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.ru'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '31vv31@inbox.ru'
+EMAIL_HOST_PASSWORD = 'dFfqP6hND5APnEscUbaU'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+ADMIN_EMAIL = 'sanfrankooo3@gmail.com'
